f3663929e9a331b9e37056c2f43e1c90d3e548f0
Bo Chen <bo.chen@kindredgroup.com>
2017-11-06 16:46:03 +1100

    localized version

--- src/RSNRadioPlayer.js
+++ src/RSNRadioPlayer.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const rsnRadioUrl = 'http://rsncast.dyn.rsn.net.au:6330/rsn';
-class FKRSNRadioPlayer extends React.Component  {
-
-    static isPlaying= false; // rsn radio is 'global'
-
+class FKRSNRadioPlayer extends React.Component{
     constructor() {
         super();
-        this.onPlayButtonClicked = this.onPlayButtonClicked.bind(this);
+        this.onPlayButtonClicked = this.onPlayButtonClicked.bind(this, Date.now());
+        this.state = {
+            soundId: 'sound_' + Date.now(),
+            isPlaying: false,
+        }
     }
     componentDidMount() {
         if (!window.soundManager) {
-            // having <script src='xx' /> inside jsx won't work becuause https://github.com/facebook/react/issues/654
             const script = document.createElement('script');
             // script.src = 'http://www.schillmania.com/projects/soundmanager2/script/soundmanager2.js';
             script.src = 'https://rsn.net.au/rsn/wp-content/plugins/compact-wp-audio-player/js/soundmanager2-nodebug-jsmin.js?ver=4.7.4';
@@ -27,9 +27,15 @@ class FKRSNRadioPlayer extends React.Component  {
             document.head.appendChild(script);
         }
     }
+    componentWillUnmount() {
+        const { soundId } = this.state;
+        const soundManager = window.soundManager;
+        soundManager.unload(soundId);
+        soundManager.destroySound(soundId);
+    }
 
     onPlayButtonClicked = () => {
-        const soundId = 'rsn_radio_live';
+        const { soundId, isPlaying } = this.state;
         const soundManager = window.soundManager;
         // sound manger will return the existing object if the same id exists
         soundManager.createSound({
@@ -38,19 +44,19 @@ class FKRSNRadioPlayer extends React.Component  {
             url: rsnRadioUrl,
         });
 
-        if (FKRSNRadioPlayer.isPlaying) {
+        if (isPlaying) {
             soundManager.pause(soundId);
         } else {
             soundManager.play(soundId);
         }       
-        FKRSNRadioPlayer.isPlaying = !FKRSNRadioPlayer.isPlaying;
-        this.forceUpdate();
+        this.setState({ isPlaying: !isPlaying });
     }
     render() {
+        const { isPlaying } = this.state;
         return <div>
             <span onClick={this.onPlayButtonClicked} >
                 LIVE RACING - Listen here
-                {FKRSNRadioPlayer.isPlaying && <span className="speaker-icon" />}
+                {isPlaying && <span className="speaker-icon" />}
             </span>
         </div >;
     }
